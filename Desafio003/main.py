3ddc9737fb904b232d28b09f2a601fc789eeb397
Amyr Kamel e Silva <67295939+amyrks1992@users.noreply.github.com>
2021-02-08 11:15:35 -0300

    Add files via upload

---
+++ Desafio003/main.py
@@ -0,0 +1,11 @@
+valor = int (input ("Digite um valor inteiro: "))
+
+if valor >0:
+  print ("elefante incomoda muita gente")
+
+
+
+
+
+
+
---
+++ Desafio004/main.py
@@ -0,0 +1,7 @@
+A = input("Digite Hello:")
+
+B = input ("Digite World:")
+
+frase = A, B #recebe os valores
+
+print (frase)
